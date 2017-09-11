146f639a187e9a16be3b794012a753ff265ff661
QHJX905 <31679232+QHJX905@users.noreply.github.com>
2017-09-11 22:35:09 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+void main()
+{
+int i,a;
+i=6;
+a=i++;
+cout<<i<<endl<<a<<endl;
+}
---
+++ 02.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+void main()
+{
+int a,b,c;
+a=5,b=5;
+c=a+b;
+cout<<"c="<<c<<endl;
+}
